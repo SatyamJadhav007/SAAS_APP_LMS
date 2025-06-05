ce6badfff16352283a5c4229a24bcf8eecfd11f8
SatyamJadhav007 <satyam.d.jadhav@gmail.com>
2025-06-05 20:33:22 +0530

    implement supabase-companion integration,clerk auth and clerk billing

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
+import { recentSessions } from "@/constants";
+//import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my saas app</h1>
-      <Button>Lets get started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#f40ea0"
+        />
+        <CompanionCard
+          id="124"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#62b652"
+        />
+        <CompanionCard
+          id="125"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#d9febd"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
   );
 };
 
